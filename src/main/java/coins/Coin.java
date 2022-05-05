8e731fa2a2a894702cd1548a77abf39c2125c7b1
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-05-05 19:35:12 +0100

    Created Coin, CoinType and Product classes

---
+++ src/main/java/coins/Coin.java
@@ -0,0 +1,19 @@
+package coins;
+
+public class Coin {
+
+    private CoinType coinType;
+
+    public Coin(CoinType coinType) {
+        this.coinType = coinType;
+    }
+
+    public CoinType getCoinType() {
+        return coinType;
+    }
+
+    public double getValue() {
+        return coinType.getValue();
+    }
+
+}
